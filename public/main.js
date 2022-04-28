2d9738a516aabec2980fef3240de78c059fbcd28
HyangsukMin <hyangsukmin503@gmail.com>
2022-04-28 23:50:09 +0900

    hyangsuk

---
+++ public/main.js
@@ -0,0 +1,10 @@
+$(function(){
+	$('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+		scrollHorizontally: true,
+		navigation: true,
+		navigationPosition: 'right',
+		sectionColor :['#ccc', '#bbb', '#ddd', '#eee']
+	});
+});
