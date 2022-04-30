baf4d9ac307dcfef581c8722dc18b22a96690e13
이영석 <2016025732@hanyang.ac.kr>
2022-05-01 05:30:16 +0900

    feat: routing for matched person

--- src/dummy/MessageDummy.js
+++ src/dummy/MessageDummy.js
@@ -13,37 +13,44 @@ export default
 [{
   content: "ㅎㅇ",
   matched: true,
-  sender: "김우연",
+  sender: "최현수",
+  senderId: "1"
 },
 {
   content: "ㅋ반가워 우리 친해지자",
   matched: true,
-  sender: "잔망루피"
+  sender: "민향숙",
+  senderId: "2"
 },
 {
   content: "우와 너 밥 좋아한다고? 나랑 맛집 찾아다니면서 놀러다니자 어때????",
   sender: "정찬이",
-  matched: false
+  matched: false,
+  senderId: "1"
 },
 {
-  content: "이거 마시면 우리,,,,",
-  sender: "뽀로로",
-  matched: true
+  content: "이거 마시면 우리,,,,친해지는거야",
+  sender: "박정재",
+  matched: true,
+  senderId: "3"
 },
 {
   content: "내가 낯을 가려서 말을 걸고 싶지만 ㅠㅠ 어렵네 소마하는 기간동안 친해졌으면 좋겠어",
   sender: "뽀로로",
-  matched: false
+  matched: false,
+  senderId: "1"
 },
 {
   content: " 반가워요:) 웃는 모습이 너무 예쁘세요 ㅎㅎ 혹시 도를 믿으세요?",
   sender: "정찬이",
-  matched: false
+  matched: false,
+  senderId: "1"
 },
 {
   content: "( ͡° ͜ʖ ͡°)",
-  sender: "복학생​",
-  matched: true
+  sender: "이주원",
+  matched: true,
+  senderId: "4"
 },
 
 ]
